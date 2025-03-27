52fc12f5ff5060d0d988a66556af74f191e65c11
Jayesh Nakum <jayesh@EKIKAs-Mac-mini.local>
2025-03-27 16:13:34 +0530

    change in home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,7 +14,17 @@ class _HomeState extends State<Home> {
   late InAppWebViewController? webViewController;
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (kIsWeb) {
+      return MaterialApp(initialRoute: '/', routes: {
+        '/': (context) => webViewPage(),
+      });
+    }
     return PopScope(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
@@ -42,7 +54,29 @@ class _HomeState extends State<Home> {
         onWebViewCreated: (controller) {
           webViewController = controller;
         },
-        initialSettings: InAppWebViewSettings(),
+        initialSettings: InAppWebViewSettings(
+          javaScriptEnabled: true,
+        ),
+        onProgressChanged: (controller, progress) {},
+        onPermissionRequest: (controller, permissionRequest) async {
+          if (permissionRequest.resources
+              .contains(PermissionResourceType.MICROPHONE)) {
+            final PermissionStatus permissionStatus =
+                await Permission.microphone.request();
+            if (permissionStatus.isGranted) {
+              return PermissionResponse(
+                resources: permissionRequest.resources,
+                action: PermissionResponseAction.GRANT,
+              );
+            } else if (permissionStatus.isDenied) {
+              return PermissionResponse(
+                resources: permissionRequest.resources,
+                action: PermissionResponseAction.DENY,
+              );
+            }
+          }
+          return null;
+        },
       ),
     );
   }
